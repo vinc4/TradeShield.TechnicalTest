bd67eef919bd61912e2bef599203c5f437bc2854
vinc4 <vincentmap@gmail.com>
2024-04-28 20:51:43 +0200

    Add Test case 2

--- TS.UnitTests/1. LongestSentanceTests.cs
+++ TS.UnitTests/1. LongestSentanceTests.cs
@@ -13,4 +13,13 @@ public class LongestSentanceTests
 
         Assert.AreEqual(4, outcome);
     }
+
+    [TestMethod]
+    public void Q1_BasicNominalTest2()
+    {
+        var input = $"Forget CVs..Save time . x x";
+        var outcome = LongestSentanceAnswer.Solution(input);
+
+        Assert.AreEqual(2, outcome);
+    }
 }
