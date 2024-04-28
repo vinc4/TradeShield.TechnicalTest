49b046da186ad7469565b4e64cbee139191d0e9b
vinc4 <vincentmap@gmail.com>
2024-04-28 20:43:56 +0200

    LongestSentanceAnswer 1st commit

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,23 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        //split string into sentences using full stop
+        string[] sentences = s.Split('.');
+        string[]? longestSentenceWords = null;
+
+        //loop trough each sentence and get the words count
+        foreach (string sentence in sentences)
+        {
+            //get words from a sentence 
+            var sentenceWords = sentence.Split(' ','!','?');
+            //check if the sentence number of words is grether than longestSentenceWords
+            if (longestSentenceWords == null || sentenceWords.Length > longestSentenceWords.Length)
+            {
+                longestSentenceWords = sentenceWords;
+            }
+        }
+
+        return longestSentenceWords.Length;
     }
+
 }
